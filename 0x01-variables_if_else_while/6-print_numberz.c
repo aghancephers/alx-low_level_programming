4f6ab698321b036ac738c202a0ec774b7bb75bbd
aghancephers <lcephers@gmail.com>
2023-03-17 20:57:14 +0000

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * Description:main - prints out a string to stdout.
+ * Return:0 if success.
+ */
+int main(void)
+{
+	int c;
+
+	c = 0;
+	while
+		(c < 10) {
+			putchar(c + '0');
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}

5937c9538ba1fc29ee874f6963ec4879f5e9f736
aghancephers <lcephers@gmail.com>
2023-03-17 20:06:30 +0000

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * Description:main - prints out a string to stdout.
+ * Return:0 if success.
+ */
+int main(void)
+{
+	char c;
+
+c = 'a';
+while
+	(c <= 'z') {
+		putchar(c);
+		c++;
+	}
+putchar('\n');
+return (0);
+}

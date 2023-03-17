18d72305ebbbf8a6e374bc2f387954c8cb108bc0
aghancephers <lcephers@gmail.com>
2023-03-17 20:48:07 +0000

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * Description:main - prints out a string to stdout.
+ * Return:0 if success.
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	printf("\n");
+	return (0);
+}

deba41a49f879c8bcc53d57b72233001af7129e0
aghancephers <lcephers@gmail.com>
2023-03-17 21:51:18 +0000

    task11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * Description:main - prints out a string to stdout.
+ * Return:0 if success.
+ */
+int main(void)
+{
+	int a, b, c;
+
+	for (a = '0'; a < '9'; a++)
+	{
+	for (b = a + 1; b <= '9'; b++)
+	{
+	for (c = b + 1; c <= '9'; c++)
+	{
+	if ((b != a) != c)
+	{
+	putchar(a);
+	putchar(b);
+	putchar(c);
+	if (a == '7' && b == '8')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}

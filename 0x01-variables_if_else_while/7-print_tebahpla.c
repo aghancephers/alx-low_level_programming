e78a4c9c0220c9d0e54b57cd4d49f7d2765e11cb
aghancephers <lcephers@gmail.com>
2023-03-17 21:04:24 +0000

    task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * Description:main - prints out a string to stdout.
+ * Return:0 if success.
+ */
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c >= 'a') {
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
+}

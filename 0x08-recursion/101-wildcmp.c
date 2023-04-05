ff5bb7222025a1b9da58e0e08327327c1fb3a250
aghancephers <lcephers@gmail.com>
2023-04-05 18:38:11 +0000

    lasttask

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * wildcmp - compares two strings containing * as a wildcard character
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: 1 if the strings can be considered identical, otherwise 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 == '\0')
+			return (1);
+		if (*s2 == '*')
+			return (wildcmp(s1, s2 + 1));
+		return (0);
+	}
+
+	if (*s2 == '*')
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	if (*s1 != *s2)
+		return (0);
+
+	return (wildcmp(s1 + 1, s2 + 1));
+}

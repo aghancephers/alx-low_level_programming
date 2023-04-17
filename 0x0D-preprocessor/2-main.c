71dc0d259006267c847232c3c246ce5294fde4f8
aghancephers <lcephers@gmail.com>
2023-04-17 12:49:48 +0000

    task2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the current source file
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* Use printf function to print the filename */
+	printf("%s\n", __FILE__);
+
+	/* Return 0 to indicate success */
+	return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,4 @@
+#ifndef FUNCTION_MACRO_H
+#define FUNCTION_MACRO_H
+#define ABS(y) ((y) < (0) ? -(y) : (y))
+#endif

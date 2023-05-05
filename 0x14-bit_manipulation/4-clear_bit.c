94a221d13efb5d4fdc59ac43d279ef7e2078339c
aghancephers <lcephers@gmail.com>
2023-05-05 21:41:33 +0000

    corrections1

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,29 +9,12 @@
 */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-unsigned long int mask = 1;
-
 /* check if index is within bounds */
 if (index > 63)
 return (-1);
 
-/* shift the 1 to the left to the index position */
-mask <<= index;
+/* use a mask to clear the bit at the given index */
+*n &= ~(1UL << index);
 
-/* while loop that shifts the 1 left until it reaches the last bit of the unsigned long int */
-while (mask <= ULONG_MAX >> 1 && mask > 0)
-{
-/* if the bit is set to 1, shift the 1 to the left by 1 bit */
-if (*n & mask)
-mask <<= 1;
-/* if the bit is set to 0, set the bit to 1 and return success */
-else
-{
-*n &= ~mask;
 return (1);
-}
-}
-
-/* if the bit is still set to 1 after all shifts, return failure */
-return (-1);
 }

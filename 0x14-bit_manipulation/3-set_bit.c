a0e7947e967640a880075ecc781c51ee5322b247
aghancephers <lcephers@gmail.com>
2023-05-05 21:36:47 +0000

    corrections

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,31 +1,21 @@
 #include "main.h"
 
 /**
-*set_bit - sets a bit at a given index to 1
-*@n: pointer to the number to change
-*@index: index of the bit to set to 1
-*
-*Return: 1 for success, -1 for failure
-*/
+ * set_bit - Sets the bit at a given index to 1.
+ *
+ * @n: Pointer to the number to change.
+ * @index: Index of the bit to set to 1.
+ *
+ * Return: 1 if successful, or -1 if an error occurred.
+ */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-unsigned long int mask = 1;
-
+/* Check if the index is out of range */
 if (index > 63)
 return (-1);
 
-mask <<= index;
+/* Use bitwise OR to set the bit at the given index */
+*n |= (1UL << index);
 
-while (mask <= ULONG_MAX >> 1 && mask > 0)
-{
-if (*n & mask)
-mask <<= 1;
-else
-{
-*n |= mask;
 return (1);
-}
-}
-
-return (-1);
 }
--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,30 +1,37 @@
 #include "main.h"
 
 /**
-*clear_bit - Sets the value of a given bit to 0.
-*@n: Pointer to the number to change.
-*@index: Index of the bit to clear.
+*clear_bit - sets the value of a given bit to 0
+*@n: pointer to the number to change
+*@index: index of the bit to clear
 *
-*Return: 1 for success, -1 for failure.
+*Return: 1 for success, -1 for failure
 */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 unsigned long int mask = 1;
 
+/* check if index is within bounds */
 if (index > 63)
 return (-1);
 
+/* shift the 1 to the left to the index position */
 mask <<= index;
 
+/* while loop that shifts the 1 left until it reaches the last bit of the unsigned long int */
 while (mask <= ULONG_MAX >> 1 && mask > 0)
 {
+/* if the bit is set to 1, shift the 1 to the left by 1 bit */
 if (*n & mask)
+mask <<= 1;
+/* if the bit is set to 0, set the bit to 1 and return success */
+else
 {
-*n ^= mask;
+*n &= ~mask;
 return (1);
 }
-mask <<= 1;
 }
 
+/* if the bit is still set to 1 after all shifts, return failure */
 return (-1);
 }
